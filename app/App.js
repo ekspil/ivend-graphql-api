158016d419ab40d770b1b9aa590a77a6e9b7486d
devnulled <devnulled@cryptaz.org>
2018-12-12 21:48:43 +0700

    Apollo: add introspection

--- app/App.js
+++ app/App.js
@@ -15,11 +15,13 @@ class App {
         }
 
 
-
         // In the most basic sense, the ApolloServer can be started
         // by passing type definitions (typeDefs) and the resolvers
         // responsible for fetching the data for those types.
-        const server = new ApolloServer({typeDefs, resolvers});
+        const server = new ApolloServer({
+            typeDefs, resolvers,
+            introspection: true
+        });
 
         // This `listen` method launches a web-server.  Existing apps
         // can utilize middleware options, which we'll discuss later.
