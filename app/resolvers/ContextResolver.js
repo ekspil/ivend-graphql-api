62fe7915c991b40adec01a385e27676f03c87e95
devnulled <devnulled@cryptaz.org>
2018-12-15 23:08:46 +0700

    ContextResolver: Basic authentication impl

--- app/resolvers/ContextResolver.js
+++ app/resolvers/ContextResolver.js
@@ -1,10 +1,12 @@
+const bcryptjs = require("bcryptjs")
+
 const parseAuthorizationHeader = async (header) => {
     const splitted = header.split(" ")
     const method = splitted[0]
     const value = splitted[1]
 
     if (method === 'basic') {
-        const credentials = new Buffer(value, 'base64')
+        const credentials = Buffer.from(value, 'base64')
             .toString('ascii')
             .split(":")
 
@@ -26,23 +28,37 @@ const resolveAuth = async (ctx) => {
 
 }
 
-module.exports = async ({req}) => {
-    const authorizationHeader = req.headers.authorization
+module.exports = function (userRepository) {
+    return async ({req}) => {
+        const authorizationHeader = req.headers.authorization
 
-    if (authorizationHeader) {
-        try {
-            const authCreds = parseAuthorizationHeader(authorizationHeader)
+        if (authorizationHeader) {
+            try {
+                const authCreds = await parseAuthorizationHeader(authorizationHeader)
 
-            const {type} = authCreds
+                const {type} = authCreds
 
-            if (type === 'basic') {
-                const {username, password} = authCreds
-                //todo try to authenticate
+                if (type === 'basic') {
+                    const {username, password} = authCreds
+
+                    const user = await userRepository.findOne({email: username})
+
+                    if (user) {
+
+                        const equals = await bcryptjs.compare(password, user.passwordHash)
+
+                        if (equals) {
+                            return {user}
+                        }
+
+                    }
+                }
+
+            } catch (e) {
 
             }
-        } catch (e) {
         }
     }
-}
 
+}
 
