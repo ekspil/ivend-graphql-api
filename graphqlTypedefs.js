a81c47d2a8bd897df6651f23df51474230863eda
devnulled <devnulled@cryptaz.org>
2018-12-15 17:56:00 +0700

    Typedefs: reformat

--- graphqlTypedefs.js
+++ graphqlTypedefs.js
@@ -1,15 +1,15 @@
 const {gql} = require('apollo-server');
 
 const typeDefs = gql`
-  type Controller {
-    uid: String
-    mode: String
-    accessKey: String
-  }
-  
-  type Query {
-    controller(uid: String!): Controller
-  }
+    type Controller {
+        uid: String
+        mode: String
+        accessKey: String
+    }
+
+    type Query {
+        controller(uid: String!): Controller
+    }
 `;
 
 module.exports = typeDefs
