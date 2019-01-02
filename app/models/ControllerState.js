e48b58bd1743bc645884ed5098deaeced9eed082
devnulled <devnulled@cryptaz.org>
2019-01-03 01:34:49 +0700

    Models: add models
    * BillAcceptorState
    * CoinAcceptorState
    * ControllerState

---
+++ app/models/ControllerState.js
@@ -0,0 +1,16 @@
+class Controller {
+    constructor(id, coinAcceptor, billAcceptor, coinAmount, billAmount, dex1Bus, dex2Bus, exeBus, mdbBus) {
+        this.id = id
+        this.coinAcceptor = coinAcceptor
+        this.billAcceptor = billAcceptor
+        this.coinAmount = coinAmount
+        this.billAmount = billAmount
+        this.dex1Bus = dex1Bus
+        this.dex2Bus = dex2Bus
+        this.exeBus = exeBus
+        this.mdbBus = mdbBus
+    }
+
+}
+
+module.exports = Controller
