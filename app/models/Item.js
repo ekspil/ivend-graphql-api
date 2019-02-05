5356210587c3102b74927dee4459b552244fb381
pshenmic <pshenmic@gmail.com>
2019-02-05 20:19:43 +0700

    Models: new models for new entities

---
+++ app/models/Item.js
@@ -0,0 +1,10 @@
+class Item {
+    constructor(id, name, price, user) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.user = user
+    }
+}
+
+module.exports = Item
---
+++ app/models/ItemMatrix.js
@@ -0,0 +1,9 @@
+class ItemMatrix {
+    constructor(id, buttons, user) {
+        this.id = id
+        this.buttons = buttons
+        this.user = user
+    }
+}
+
+module.exports = ItemMatrix
