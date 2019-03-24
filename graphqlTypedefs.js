8c0ca4d6a3082a56a2feabfd84ccb7ca9c1cd6e0
admin <admin@illspill.org>
2019-03-24 16:38:14 +0300

    GraphQL: make equipment of type Controller nullable

--- graphqlTypedefs.js
+++ graphqlTypedefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
     type Controller {
         id: Int!
         name: String!
-        equipment: Equipment!
+        equipment: Equipment
         uid: String!
         revision: Revision!
         status: ControllerStatus!
