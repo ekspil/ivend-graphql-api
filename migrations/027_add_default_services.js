4398722997e0ce284091ec384dffc64512caa02e
devnulled <devnulled@cryptaz.org>
2019-04-15 15:21:57 +0700

    Migrations: add default services

---
+++ migrations/027_add_default_services.js
@@ -0,0 +1,17 @@
+"use strict"
+
+module.exports = {
+    up: async (queryInterface) => {
+        await queryInterface.bulkInsert("services", {
+            name: "Telemetry",
+            price: 100,
+            billingType: "MONTHLY",
+            type: "CONTROLLER",
+            created_at: new Date(),
+            updated_at: new Date()
+        })
+    },
+    down: async () => {
+        throw new Error("All failed migrations have to be resolved manually")
+    }
+}
