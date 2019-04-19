e2168c2c2fbdb86f60c76822399ec46240a6f735
devnulled <devnulled@cryptaz.org>
2019-04-19 20:21:37 +0700

    Sequelize: add MachineLog model

---
+++ app/models/sequelize/MachineLog.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize")
+
+const Machine = {
+    id: {
+        type: Sequelize.DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    message: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: false
+    },
+    createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true,
+        unique: false,
+        field: "created_at"
+    },
+    updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true,
+        unique: false,
+        field: "updated_at"
+    },
+}
+module.exports = Machine
