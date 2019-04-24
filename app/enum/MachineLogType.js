5cd40796f050bdbdc87012028ce6eed05293f37a
devnulled <devnulled@cryptaz.org>
2019-04-24 16:53:40 +0700

    MachineLog: enum for log types

---
+++ app/enum/MachineLogType.js
@@ -0,0 +1,9 @@
+const MachineLogType = {
+    "CONNECTION": "ENABLED",
+    "COINACCEPTOR": "COINACCEPTOR",
+    "BILLACCEPTOR": "BILLACCEPTOR",
+    "BUS_ERROR": "BUS_ERROR",
+    "REGISTRATION": "REGISTRATION"
+}
+
+module.exports = MachineLogType
