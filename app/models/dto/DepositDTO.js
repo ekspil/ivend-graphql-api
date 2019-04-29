3edfe533e28b0ceaec84862b04d67beb1891063d
devnulled <devnulled@cryptaz.org>
2019-04-29 16:41:39 +0700

    DepositDTO: remove mapper

--- app/models/dto/DepositDTO.js
+++ app/models/dto/DepositDTO.js
@@ -1,5 +1,3 @@
-const UnknownPaymentStatus = require("../../errors/UnknownPaymentStatus")
-
 class DepositDTO {
 
     constructor({id, amount, status, redirectUrl, timestamp}) {
@@ -7,21 +5,7 @@ class DepositDTO {
         this.amount = amount
         this.redirectUrl = redirectUrl
         this.timestamp = timestamp
-
-        switch (status) {
-            case "pending":
-            case "waiting_for_capture":
-                this.status = "PENDING"
-                break
-            case "succeeded":
-                this.status = "SUCCEEDED"
-                break
-            case "canceled":
-                this.status = "CANCELLED"
-                break
-            default:
-                throw new UnknownPaymentStatus
-        }
+        this.status = status
     }
 }
 
