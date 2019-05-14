fd3381a14e4e7b6d59261331e9c56f81dd678a21
ekspil <ekspil@yandex.ru>
2019-05-14 22:54:51 +1000

    KktDTO: fix

--- app/models/dto/KktDTO.js
+++ app/models/dto/KktDTO.js
@@ -3,16 +3,16 @@
 class KktDTO {
 
     constructor({id, kktModel, kktFactoryNumber, kktRegNumber, kktFNNumber, kktActivationDate, kktBillsCount, kktOFDRegKey, inn, companyName}) {
-        this.kkt.id = id
-        this.kkt.inn = inn
-        this.kkt.companyName = companyName
-        this.kkt.kktModel = kktModel
-        this.kkt.kktFactoryNumber = kktFactoryNumber
-        this.kkt.kktRegNumber = kktRegNumber
-        this.kkt.kktFNNumber = kktFNNumber
-        this.kkt.kktActivationDate = kktActivationDate
-        this.kkt.kktBillsCount = kktBillsCount
-        this.kkt.kktOFDRegKey = kktOFDRegKey
+        this.id = id
+        this.inn = inn
+        this.companyName = companyName
+        this.kktModel = kktModel
+        this.kktFactoryNumber = kktFactoryNumber
+        this.kktRegNumber = kktRegNumber
+        this.kktFNNumber = kktFNNumber
+        this.kktActivationDate = kktActivationDate
+        this.kktBillsCount = kktBillsCount
+        this.kktOFDRegKey = kktOFDRegKey
     }
 }
 
