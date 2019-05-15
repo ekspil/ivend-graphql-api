d8b13e67155814b06c4f42e4841b8b2e88cfa09e
ekspil <ekspil@yandex.ru>
2019-05-16 00:13:33 +1000

    Разделил миграцию на 2 файла

--- migrations/042_kkt_change_fnnumber.js
+++ migrations/042_kkt_change_fnnumber.js
@@ -2,8 +2,11 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.addColumn("kkts", "kktRegNumber", Sequelize.TEXT)
-
+        await queryInterface.changeColumn("kkts", "kktFNNumber", {
+            type: Sequelize.TEXT,
+            allowNull: true,
+            unique: true
+        })
     },
     down: async () => {
         throw new Error("All failed migrations have to be resolved manually")
