5a5fab2bfee16535fc4df4687d01fb0c813343a8
devnulled <devnulled@cryptaz.org>
2019-08-30 01:26:20 +0700

    FiscalService: fix getFiscalString

--- app/services/FiscalService.js
+++ app/services/FiscalService.js
@@ -1,27 +1,25 @@
 module.exports = {
 
     /**
-     *
+     *20190704T013736
      * @param receipt {FiscalReceiptDTO}
      * @returns {string}
      */
     getFiscalString: function (receipt) {
         const {sno} = receipt
         const {fiscalData} = receipt
-        const {receiptDatetime, totalAmount, fnNumber, fiscalDocumentNumber, fiscalDocumentAttribute} = fiscalData
+        const {totalAmount, fnNumber, fiscalDocumentNumber, fiscalDocumentAttribute} = fiscalData
 
-        let t_arr = receiptDatetime.split(" ")
-        let t_date = t_arr[0].split(".")
-        t_date = t_date.reverse()
-        t_date = "" + t_date[0] + t_date[1] + t_date[2]
-        let t_time = t_arr[1].split(":")
-        t_time = "" + t_time[0] + t_time[1] + t_time[2]
-        let t = t_date + "T" + t_time
-        let s = totalAmount.total.toFixed(2)
-        let fn = fnNumber
-        let i = fiscalDocumentNumber
-        let fp = fiscalDocumentAttribute
+        const receiptDatetime = new Date(fiscalData.receiptDatetime)
+
+        const t = `${receiptDatetime.getFullYear()}${receiptDatetime.getMonth() + 1}${receiptDatetime.getDate()}T${receiptDatetime.getHours()}${receiptDatetime.getMinutes()}${receiptDatetime.getSeconds()}`
+        const s = totalAmount
+        const fn = fnNumber
+        const i = fiscalDocumentNumber
+        const fp = fiscalDocumentAttribute
+
         let n = 1
+
         switch (sno) {
             case "usn_income":
                 n = 1
