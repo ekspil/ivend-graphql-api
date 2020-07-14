898638c14c540e66ef06a328d1e20db147778739
Aleksey Efremov <ekspil@yandex.ru>
2020-07-14 14:18:06 +1000

    fix

--- app/enum/RolePermissions.js
+++ app/enum/RolePermissions.js
@@ -65,7 +65,6 @@ const VENDOR = [
     Permission.GET_RECEIPT,
     Permission.SEND_EMAIL,
 
-
 ]
 
 const VENDOR_NOT_CONFIRMED = [
@@ -80,12 +79,50 @@ const VENDOR_NOT_CONFIRMED = [
 
 const VENDOR_NEGATIVE_BALANCE = [
     Permission.GET_SELF_DEPOSITS,
+    Permission.GET_NEWS,
     Permission.GET_DAILY_BILL,
     Permission.GET_DAYS_LEFT,
     Permission.GET_BALANCE,
     Permission.REQUEST_DEPOSIT,
+    Permission.GET_ALL_CONTROLLERS_OF_CURRENT_USER,
+    Permission.GET_CONTROLLER_BY_ID,
+    Permission.GET_CONTROLLER_BY_UID,
+    Permission.GET_CONTROLLER_ERRORS,
+    Permission.FIND_EQUIPMENT_BY_ID,
+    Permission.GET_ALL_EQUIPMENTS,
+    Permission.GET_ITEM_MATRIX_BY_ID,
+    Permission.GET_ITEM_BY_ID,
+    Permission.GET_USER_ITEMS,
+    Permission.FIND_ALL_NOTIFICATION_SETTINGS,
+    Permission.FIND_NOTIFICATION_SETTING_BY_TYPE,
+    Permission.GET_ALL_REVISIONS,
+    Permission.GET_REVISION_BY_ID,
+    Permission.GET_LAST_SALE,
+    Permission.GET_LAST_SALE_OF_ITEM,
+    Permission.GET_ITEM_SALE_STATS,
+    Permission.GET_SALES_SUMMARY,
+    Permission.FIND_SERVICE_BY_ID,
+    Permission.GET_SERVICES_FOR_CONTROLLER,
+    Permission.GET_CONTROLLER_SERVICES,
     Permission.GET_PROFILE,
-    Permission.GET_DEPOSIT_BY_ID
+    Permission.GET_MACHINE_GROUP_BY_ID,
+    Permission.GET_MACHINE_TYPE_BY_ID,
+    Permission.GET_ALL_SELF_MACHINES,
+    Permission.GET_ALL_SELF_MACHINE_GROUPS,
+    Permission.GET_ALL_MACHINE_TYPES,
+    Permission.GET_MACHINE_BY_ID,
+    Permission.GET_MACHINE_BY_CONTROLLER_ID,
+    Permission.GET_DEPOSIT_BY_ID,
+    Permission.CONFIRM_USER_ACTION,
+    Permission.EDIT_EMAIL,
+    Permission.EDIT_PASSWORD,
+    Permission.GET_MACHINE_ENCASHMENTS,
+    Permission.GET_KKT_BY_ID,
+    Permission.GET_USER_KKTS,
+    Permission.GET_LEGAL_INFO,
+    Permission.GET_SALES,
+    Permission.GET_RECEIPT,
+    Permission.SEND_EMAIL,
 ]
 
 const VENDOR_NO_LEGAL_INFO = [
