92c8aa7cb005ad8253757dd49d368720b18a1b76
Aleksey Efremov <ekspil@yandex.ru>
2020-12-09 18:55:59 +1000

    fix

---
+++ migrations/073_change_partner_in_userstable.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+
+        await queryInterface.removeColumn("users", "partner_id")
+        await queryInterface.addColumn("users", "partner_id",
+            {
+                type: Sequelize.BIGINT
+            })
+    },
+    down: async () => {
+        throw new Error("All failed migrations have to be resolved manually")
+    }
+}
