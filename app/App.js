e395d9dcc6ae6642fdedac1c2d8fb5b94aff29e6
pshehmic <pshenmic@gmail.com>
2021-05-24 01:11:18 +0700

    Add redis name

--- app/App.js
+++ app/App.js
@@ -74,7 +74,7 @@ const redis = new Redis({
     sentinels: [
         {host: process.env.REDIS_HOST, port: process.env.REDIS_PORT}
     ],
-    name: "redis",
+    name: process.env.REDIS_NAME,
     password: process.env.REDIS_PASSWORD,
     role: "master"
 })
