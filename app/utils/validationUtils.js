24224ab49d3c1cbe944b55dc8c7f2933cbcda5ad
Efremov AV <ekspil@yandex.ru>
2021-08-10 10:35:00 +1000

    terminal mods

--- app/utils/validationUtils.js
+++ app/utils/validationUtils.js
@@ -1,5 +1,5 @@
 function validatePhoneNumber(phone) {
-    return typeof phone === "string" && new RegExp(/^9(\d){9,9}$/).test(phone)
+    return typeof phone === "string" && new RegExp(/^[7,9](\d){9,9}$/).test(phone)
 }
 
 
